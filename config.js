c5729ccf7aa74c89d6a7c6754c338c78437eb9ed
worldcoininvestmentco-star <worldcoininvestment.co@gmail.com>
2025-09-28 18:10:26 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  username: process.env.PANEL_USER || 'Tomi',
-  password: process.env.PANEL_PASS || 'lucky',
+  username: process.env.PANEL_USER || 'admin',
+  password: process.env.PANEL_PASS || 'changeme',
   statsPath: process.env.STATS_URL || process.env.STATS_PATH || './stats.json',
   sessionSecret: process.env.SESSION_SECRET || 'please-change-this',
   pollIntervalMs: Number(process.env.POLL_INTERVAL_MS || 5000)
